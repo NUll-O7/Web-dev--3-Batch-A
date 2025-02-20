7611952db216c5c2826f5b36a16f42ad28cf9aa2
mrinal1224 <iammrinalkc@gmail.com>
2025-02-20 11:25:21 +0530

    push

--- Class-4_5 (Reat Build Tools and Props)/app/vite-project/src/components/Form.jsx
+++ Class-4_5 (Reat Build Tools and Props)/app/vite-project/src/components/Form.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Form() {
+
+    const [name , setName] = useState('')
+    const [password , setPassword] = useState('')
+    const [email , setEmail] = useState('')
+
+    // function onNameChange(e){
+    //   setName(e.target.value)
+    // }
+
+    function submitForm(e){
+      e.preventDefault()
+      console.log(name)
+      console.log(email)
+      console.log(password)
+
+    }
+
   return (
-    <form>
+    <form onSubmit={submitForm}>
       <label>Name :</label>
-      <input type="text" />
+      <input type="text" value={name} onChange={(e)=>setName(e.target.value)}  />
 
       <label>Email :</label>
-      <input type="email" />
+      <input type="email"  value={email} onChange={(e)=>setEmail(e.target.value)} />
 
       <label>password :</label>
-      <input type="password" />
+      <input type="password"  value={password} onChange={(e)=>setPassword(e.target.value)} />
 
       <button type="submit">Submit From</button>
     </form>
