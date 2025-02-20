d49914be457b71ea922ef66da5107109e97ed523
mrinal1224 <iammrinalkc@gmail.com>
2025-02-20 10:57:13 +0530

    push

--- Class-4_5 (Reat Build Tools and Props)/app/vite-project/src/App.jsx
+++ Class-4_5 (Reat Build Tools and Props)/app/vite-project/src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
 import Card from "./components/Card";
+import Counter from "./components/Counter";
 import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <Card title='Apple' description="Mackbook Pro 2020"/>
       <Card title='Nike' description='Shoes'/>
       <Card title='Home Applicance' description='Television'/>
-      <Card title='Sunglasses' description='Rayban Glasses' />
+      <Card title='Sunglasses' description='Rayban Glasses' /> */}
+      <Counter/>
     </>
   );
 }
---
+++ Class-4_5 (Reat Build Tools and Props)/app/vite-project/src/components/Counter.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+
+function Counter() {
+
+ const [count , setCount] = useState(0)
+
+ function increment(){
+    setCount(count+1)
+ }
+
+  return (
+    <div>
+    <button onClick={increment}>Increment</button>
+    <h1>{count}</h1>
+    <button>Decrement</button>
+    </div>
+  )
+}
+
+export default Counter
