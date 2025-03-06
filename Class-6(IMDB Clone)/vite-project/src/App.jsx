6774b4df3e3551b4a744385f4fa03934d100fe51
mrinal1224 <iammrinalkc@gmail.com>
2025-03-06 13:00:53 +0530

    push

--- Class-6(IMDB Clone)/vite-project/src/App.jsx
+++ Class-6(IMDB Clone)/vite-project/src/App.jsx
@@ -1,11 +1,24 @@
-import './App.css'
+import "./App.css";
+import Movies from "./components/Movies";
+import NavBar from "./components/Navbar";
+import Recommendation from "./components/Recommendation";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return(
+  return (
     <div>
-    <h1 className='text-red-600 text-3xl font-bold underline'>Hello from React</h1>
+      <BrowserRouter>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+          <Route path="/recommend" element={<Recommendation />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
