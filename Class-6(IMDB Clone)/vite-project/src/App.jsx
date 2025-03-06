6c2df1de2438be1bf8121bc81eff5d257e868df8
mrinal1224 <iammrinalkc@gmail.com>
2025-03-06 10:40:17 +0530

    push

---
+++ Class-6(IMDB Clone)/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return(
+    <div>
+    <h1 className='text-red-600 text-3xl font-bold underline'>Hello from React</h1>
+    </div>
+  )
+}
+
+export default App
