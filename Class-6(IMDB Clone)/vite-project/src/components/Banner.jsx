85ac5bdeb634ae5d0118b87f9dd430375646a181
mrinal1224 <iammrinalkc@gmail.com>
2025-03-18 10:28:17 +0530

    push

--- Class-6(IMDB Clone)/vite-project/src/components/Banner.jsx
+++ Class-6(IMDB Clone)/vite-project/src/components/Banner.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Banner() {
   return (
-    <div>This is the Banner</div>
+    <div>
+      <div
+        className="h-[70vh] w-[100vw] bg-cover"
+        style={{
+          backgroundImage:
+            "url(https://i.pinimg.com/originals/29/7d/e0/297de0761b0c756266d74ca50d03cc1d.jpg)",
+        }}
+      ></div>
+    </div>
   )
 }
 
