6ff5720d0b69865566fc7df3e76b65b3ae96bcda
mrinal1224 <iammrinalkc@gmail.com>
2025-03-18 11:30:01 +0530

    push

--- Class-6(IMDB Clone)/vite-project/src/components/Movies.jsx
+++ Class-6(IMDB Clone)/vite-project/src/components/Movies.jsx
@@ -1,24 +1,40 @@
-import React from 'react'
-import MovieCard from './MovieCard'
+// https://api.themoviedb.org/3/movie/popular?language=en-US&page=1
+
+import React, { useEffect } from "react";
+import MovieCard from "./MovieCard";
+import axios from 'axios'
 
 function Movies() {
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=3aec63790d50f3b9fc2efb4c15a8cf99&language=en-US&page=2`
+      )
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
-       <div className="text-2xl font-bold text-center m-5">
-         <h1>Trending Movies</h1>
-       </div>
-       <div className="flex justify-evenly flex-wrap gap-8  ">
-         <MovieCard />
-         <MovieCard />
-         <MovieCard />
-         <MovieCard />
-         <MovieCard />
-         <MovieCard />
-         <MovieCard />
-         <MovieCard />
-       </div>
-     </div>
-  )
+      <div className="text-2xl font-bold text-center m-5">
+        <h1>Trending Movies</h1>
+      </div>
+      <div className="flex justify-evenly flex-wrap gap-8  ">
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </div>
+    </div>
+  );
 }
 
-export default Movies
+export default Movies;
