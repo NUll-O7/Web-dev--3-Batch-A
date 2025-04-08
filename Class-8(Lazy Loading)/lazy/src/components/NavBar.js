9593b5e481fce991aabd13f3e3cb20ba06c55cc4
mrinal1224 <iammrinalkc@gmail.com>
2025-04-08 11:46:04 +0530

    Lazy Loading and code Splitting

---
+++ Class-8(Lazy Loading)/lazy/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import {Link} from "react-router-dom"
+
+function Navbar() {
+
+return (
+    <div style={{margin:'3rem', display:'flex', justifyContent:'center'}}>
+     <Link style={{margin:'2rem'}} to='/'>Home</Link>
+     <Link style={{margin:'2rem'}} to='/about'>About</Link>
+     <Link style={{margin:'2rem'}} to='/contact'>contact</Link>
+
+    </div>
+  );
+}
+
+export default Navbar;
