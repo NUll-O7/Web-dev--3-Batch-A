394ed28e05fc4cfa93f33b9b37dde0ecf65651ca
mrinal1224 <iammrinalkc@gmail.com>
2025-04-08 10:54:56 +0530

    push

--- Class-8(Lazy Loading)/lazy/src/components/Books.js
+++ Class-8(Lazy Loading)/lazy/src/components/Books.js
@@ -1,21 +1,19 @@
-import React, { useState } from 'react'
-import { books } from './booksData'
-
+import React, { useState } from "react";
+import { books } from "./booksData";
 
 function Books() {
-  const [booksData , setBooks] = useState(null)
+  const [booksData, setBooks] = useState(null);
 
-  function getData(){
-    setBooks(JSON.stringify(books))
+  function getData() {
+    setBooks(JSON.stringify(books));
   }
 
- 
   return (
     <>
-    <button onClick={getData}>Get Books Data</button>
-    <div>{booksData}</div>
+      <button onClick={getData}>Get Books Data</button>
+      <div>{booksData}</div>
     </>
-  )
+  );
 }
 
-export default Books
+export default Books;
