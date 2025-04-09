c3b94335d9173de4ba3c12ba2bbc34f28f4c5dc5
mrinal1224 <iammrinalkc@gmail.com>
2025-04-09 18:53:05 +0530

    useRef

--- Class-10(React Optimization)/vite-project/src/components/Ref.jsx
+++ Class-10(React Optimization)/vite-project/src/components/Ref.jsx
@@ -3,13 +3,23 @@ import React, { useState, useRef } from "react";
 function Ref() {
   const [input, setInput] = useState("");
 
-  function reset() {}
+  function reset() {
+    setInput('')
+     refElement.current.focus()
+     refElement.current.style.color = 'red'
+       refElement.current.style.backgroundColor = 'white'
+  }
+
+   let refElement = useRef('')
+   console.log(refElement)
 
   return (
     <div>
       <h1>Use Ref</h1>
 
       <input
+      ref={refElement} 
+     
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
